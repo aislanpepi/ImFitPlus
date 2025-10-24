070555ca4f24921d9cff9ead301b72eea373d6f6
Aislan <aislan.pepi@outlook.com>
2025-10-23 21:22:27 -0300

    feat: create class and xml from activity result caloric form

--- app/src/main/java/br/edu/ifsp/scl/prdm/sc3038939/imfitplus/ResultCaloricActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/prdm/sc3038939/imfitplus/ResultCaloricActivity.kt
@@ -1,4 +1,17 @@
 package br.edu.ifsp.scl.prdm.sc3038939.imfitplus
 
-class ResultCaloric {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.scl.prdm.sc3038939.imfitplus.databinding.ActivityResultFormBinding
+import br.edu.ifsp.scl.prdm.sc3038939.imfitplus.databinding.ActivityResultGastoBinding
+
+class ResultCaloricActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityResultGastoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityResultGastoBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
 }
