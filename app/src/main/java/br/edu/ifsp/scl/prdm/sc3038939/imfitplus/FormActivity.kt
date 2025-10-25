ad00a4f79876a6becf2e625809109373fa53f7ba
Aislan <aislan.pepi@outlook.com>
2025-10-25 15:55:17 -0300

    feat: add logic to pass the data from formactivity to resultformactivity

--- app/src/main/java/br/edu/ifsp/scl/prdm/sc3038939/imfitplus/FormActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/prdm/sc3038939/imfitplus/FormActivity.kt
@@ -1,11 +1,16 @@
 package br.edu.ifsp.scl.prdm.sc3038939.imfitplus
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import br.edu.ifsp.scl.prdm.sc3038939.imfitplus.databinding.ActivityFormBinding
+import java.lang.Math.pow
+import kotlin.math.pow
 
 class FormActivity : AppCompatActivity() {
 
@@ -15,5 +20,24 @@ class FormActivity : AppCompatActivity() {
         binding = ActivityFormBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val spinner: Spinner = binding.spinnerAtividade
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.niveis_atividade_fisica,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
+        binding.btCalculoImc.setOnClickListener {
+            val imc = binding.etPeso.text.toString().toFloat() / binding.etAltura.text.toString().toDouble().pow(2.0)
+            val resultFormIntent = Intent(this, ResultFormActivity::class.java)
+            resultFormIntent.putExtra("nome_completo",binding.etNomeCompleto.text.toString())
+            resultFormIntent.putExtra("imc",imc)
+            resultFormIntent.putExtra("nivel_atividade",binding.spinnerAtividade.selectedItem.toString())
+            startActivity(resultFormIntent)
+        }
     }
 }
--- app/src/main/java/br/edu/ifsp/scl/prdm/sc3038939/imfitplus/ResultFormActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/prdm/sc3038939/imfitplus/ResultFormActivity.kt
@@ -1,6 +1,9 @@
 package br.edu.ifsp.scl.prdm.sc3038939.imfitplus
 
+import android.icu.text.DecimalFormat
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.scl.prdm.sc3038939.imfitplus.databinding.ActivityResultFormBinding
 
@@ -8,9 +11,27 @@ class ResultFormActivity: AppCompatActivity() {
     private lateinit var binding: ActivityResultFormBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val imcFormat = DecimalFormat("##.##")
+        val nome = intent.extras?.getString("nome_completo")
+        val imc = intent.extras?.getDouble("imc")
+        val nivelAtividade = intent.extras?.getString("nivel_atividade")
+
+
         super.onCreate(savedInstanceState)
         binding = ActivityResultFormBinding.inflate(layoutInflater)
         val view = binding.root
+        binding.tvNome.text = nome
+        binding.tvImc.text = imcFormat.format(imc)
+        binding.tvCategoriaImc.text = getImcCategory(imcFormat.format(imc))
         setContentView(view)
+    }
+
+    fun getImcCategory(imc: String): String {
+        val imcValue = imc.toDouble()
+
+        if(imcValue >= 30) return "Obesidade"
+        if(imcValue >= 25) return "Sobrepeso"
+        if(imcValue >= 18.5) return "Normal"
+        return "Abaixo do peso"
     }
 }
