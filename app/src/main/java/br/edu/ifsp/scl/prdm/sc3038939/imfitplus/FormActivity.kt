726d73c1c42617856650b766b9f2e39c4109206a
Aislan <aislan.pepi@outlook.com>
2025-10-31 01:00:22 -0300

    feat: adicionado tratamento de exceções e validadção de dados

--- app/src/main/java/br/edu/ifsp/scl/prdm/sc3038939/imfitplus/FormActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/prdm/sc3038939/imfitplus/FormActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -31,17 +32,44 @@ class FormActivity : AppCompatActivity() {
         }
 
         binding.btCalculoImc.setOnClickListener {
-            val imc = binding.etPeso.text.toString().toDouble() / binding.etAltura.text.toString().toDouble().pow(2.0)
+            val nome = binding.etNomeCompleto.text.toString().trim()
+            val idadeStr = binding.etIdade.text.toString().trim()
+            val pesoStr = binding.etPeso.text.toString().trim()
+            val alturaStr = binding.etAltura.text.toString().trim()
+
             val resultFormIntent = Intent(this, ResultFormActivity::class.java)
+
+            if(nome.isEmpty()) {
+                Toast.makeText(this,"Campo nome não pode ser vazio",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if(idadeStr.isEmpty() || idadeStr.toInt() <= 0 || idadeStr.toInt() > 150){
+                Toast.makeText(this,"Campo idade é invalido.",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if(idadeStr.isEmpty() || pesoStr.toDouble() <= 0){
+                Toast.makeText(this,"Campo peso é invalido.",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if(alturaStr.toDouble() <= 0){
+                Toast.makeText(this,"Campo altura é invalido.",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
             val usuario = Bundle()
 
-            usuario.putString("nome_completo",binding.etNomeCompleto.text.toString())
-            usuario.putDouble("imc",imc)
+            usuario.putString("nome_completo",nome)
+            usuario.putInt("idade", idadeStr.toInt())
+            usuario.putDouble("peso",pesoStr.toDouble())
+            usuario.putString("sexo",if(binding.rbMasculino.isChecked) binding.rbMasculino.text.toString()
+                                    else binding.rbFeminino.text.toString())
+            usuario.putDouble("altura",alturaStr.toDouble())
             usuario.putString("nivel_atividade",binding.spinnerAtividade.selectedItem.toString())
-            usuario.putInt("idade", binding.etIdade.text.toString().toInt())
-            usuario.putString("sexo",if(binding.rbMasculino.isChecked) binding.rbMasculino.text.toString() else binding.rbFeminino.text.toString())
-            usuario.putDouble("peso",binding.etPeso.text.toString().toDouble())
-            usuario.putDouble("altura",binding.etAltura.text.toString().toDouble())
+            val imc = pesoStr.toDouble() / alturaStr.toDouble().pow(2.0)
+            usuario.putDouble("imc",imc)
+
             resultFormIntent.putExtras(usuario)
 
             startActivity(resultFormIntent)
