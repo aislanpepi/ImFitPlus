a03cbbe13d93d8b48830a9917752f2ac272605b9
Aislan <aislan.pepi@outlook.com>
2025-10-31 00:16:14 -0300

    feat: adicionado logica ao IdealWeightActivity

--- app/src/main/java/br/edu/ifsp/scl/prdm/sc3038939/imfitplus/IdealWeightActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/prdm/sc3038939/imfitplus/IdealWeightActivity.kt
@@ -13,6 +13,9 @@ class IdealWeightActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityResultIdealWeightBinding.inflate(layoutInflater)
         val view = binding.root
+        val extras = intent.extras!!
+        binding.tvNome.text = getString(R.string.nome_completo_variable,extras.getString("nome_completo"))
+        binding.tvPesoIdeal.text = extras.getDouble("peso_ideal",0.0).toString()
         setContentView(view)
     }
 }
--- app/src/main/java/br/edu/ifsp/scl/prdm/sc3038939/imfitplus/ResultCaloricActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/prdm/sc3038939/imfitplus/ResultCaloricActivity.kt
@@ -1,9 +1,12 @@
 package br.edu.ifsp.scl.prdm.sc3038939.imfitplus
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.scl.prdm.sc3038939.imfitplus.databinding.ActivityResultFormBinding
 import br.edu.ifsp.scl.prdm.sc3038939.imfitplus.databinding.ActivityResultGastoBinding
+import java.lang.Math.pow
+import kotlin.math.pow
 
 class ResultCaloricActivity: AppCompatActivity() {
     private lateinit var binding: ActivityResultGastoBinding
@@ -17,7 +20,15 @@ class ResultCaloricActivity: AppCompatActivity() {
         binding.tvNome.text = getString(R.string.nome_completo_variable,extras.getString("nome_completo"))
         binding.tvGastoCalorico.text = getString(R.string.gasto_calorico_ideal,extras.getDouble("gasto_calorico"))
 
-
         setContentView(view)
+
+        val pesoIdeal = 22 * (extras.getDouble("altura").pow(2.0))
+
+        binding.btCalculoPesoIdeal.setOnClickListener{
+            val calculoPeso = Intent(this, IdealWeightActivity::class.java)
+            extras.putDouble("peso_ideal",pesoIdeal)
+            calculoPeso.putExtras(extras)
+            startActivity(calculoPeso)
+        }
     }
 }
